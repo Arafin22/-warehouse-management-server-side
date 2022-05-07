f104b5fcdc85053b00e9631b6c69bd639a735147
arafin0 <sultanularafin000@gmail.com>
2022-05-07 17:04:09 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fiojy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("db connect");
+    const productCollection = client
+      .db("booksInventory")
+      .collection("products");
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
